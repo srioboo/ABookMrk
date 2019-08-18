dfc7ecb0cba882e83088b847287cfc01ea39c009
Salva Rioboo <salrio74@gmail.com>
2019-08-18 13:25:39 +0200

    Primera subida a GITHUB

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import 'bootstrap';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'ABookMrk';
+
+  ngOnInit(): void {
+    $(document).ready(() => {
+      console.log('test');
+      // $('#elementId').css({'background-color': 'yellow', 'font-size': '200%'});
+    });
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
