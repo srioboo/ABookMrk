61030f8641ac5e8d0b9ed194f781834ead1901c6
Salva Rioboo <salrio74@gmail.com>
2019-09-09 16:00:37 +0200

    se separa los json del codigo

--- src/app/mock-bookmark.ts
+++ src/app/mock-bookmark.ts
@@ -1,29 +1,4 @@
 import { Bookmark } from './bookmark';
+import * as jsonbookmarks from './jsonentorno.json';
 
-export const BOOKMARKS: Bookmark[] = [
-  {
-    name: 'Facebook',
-    url: 'http://www.facebook.com'
-  }, {
-    name: 'Feedly',
-    url: 'http://www.feedly.com'
-  }, {
-    name: 'Twiter',
-    url: 'http://www.twitter.com'
-  }, {
-    name: 'Pocket',
-    url: 'http://www.getpocket.com/a/queuque/list'
-  }, {
-    name: 'Netflix',
-    url: 'http://www.netflix.com'
-  }, {
-    name: 'Twiter',
-    url: 'http://www.twitter.com'
-  }, {
-    name: 'Calendario Series',
-    url: 'https://www.pogdesign.co.uk/cat/'
-  }, {
-    name: 'Just Watch',
-    url: 'https://www.justwatch.com/es'
-  }
-];
+export const BOOKMARKS: Bookmark[] = jsonbookmarks;
--- src/app/mock-navegacion.ts
+++ src/app/mock-navegacion.ts
@@ -1,12 +1,4 @@
 import { Navegacion } from './navegacion';
+import * as jsonnavegacion from './jsonnavegacion.json';
 
-export const NAVIGATION: Navegacion[] = [
-    {
-        id: 1, nombre: 'IBM-WC V9',
-        url: 'https://www.ibm.com/support/knowledgecenter/en/SSZLC2_9.0.0/landing/wc_welcome.htm', descripcion: ''
-    },
-    {
-        id: 2, nombre: 'IBM-WC V8',
-        url: 'https://www.ibm.com/support/knowledgecenter/en/SSZLC2_8.0.0/landing/wc_welcome.htm', descripcion: ''
-    }
-];
+export const NAVIGATION: Navegacion[] = jsonnavegacion;
